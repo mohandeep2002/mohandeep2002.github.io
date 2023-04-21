0347e5580194a0ecf6394f733ef4ac32abace335
Mohandeep <mohandeep2002@gmail.com>
2023-04-21 12:42:39 +0530

    emailjs added

--- js/index.js
+++ js/index.js
@@ -1,25 +1,15 @@
-const nodemailer = require('nodemailer');
+function sendEmail(){
+  const emailParams = {
+    to_name: "Mohandeep Checking",
+    from_name: "Maddy",
+    message: "Test email"
+  };
+  emailjs.send("service_9j3inip","template_w2j64pe", emailParams)
+    .then(function(response) {
+      console.log("Email sent successfully", response);
+    })
+    .catch(function(error) {
+      console.error("Failed to send email", error);
+    });
+}
 
-let transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: '190030107cse@gmail.com',
-    pass: 'euoyufqazusggyze'
-  }
-});
-
-let mailOptions = {
-  from: '190030107cse@gmail.com',
-  to: 'mohandeep2002@gmail.com',
-  subject: 'Test Email',
-  text: 'This is a test email sent using SMTP from Node.js'
-};
-
-transporter.sendMail(mailOptions, function(error, info) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
-document.getElementById('staticbutton').addEventListener('click', sendEmail);
